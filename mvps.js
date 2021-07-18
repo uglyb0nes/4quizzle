c2ad63f9b1d1136e55068019034de8914275bffe
Leigh Johnson <leigh.carterjohnson@gmail.com>
2021-07-17 17:27:25 -0700

    final touches

--- mvps.js
+++ mvps.js
@@ -1,6 +1,5 @@
 var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
-var goBack = document.querySelector("#goBack");
 
 clear.addEventListener("click", function () {
     localStorage.clear();
